1d0ebb8a0ba35a4c3478d22d290e45de218c1b79
Grigory Rudko <Rudko@users.noreply.github.com>
2016-09-06 18:14:07 -0700

    Add files via upload

--- testingSnapchatCamera/ViewController.swift
+++ testingSnapchatCamera/ViewController.swift
@@ -12,11 +12,28 @@ import AVFoundation
 
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
-    
-    @IBOutlet weak var billField: UITextField!
+   
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+    
+    }
+    
     
     
     var captureSession: AVCaptureSession?
@@ -31,22 +48,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
     }
 
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
-    }
+   
     
-    @IBAction func calculateTip(sender: AnyObject) {
-        
-        let tipPercentages = [0.18, 0.2, 0.25]
-        
-        let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        
-    }
+   
     
     
     
@@ -94,4 +98,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
+
+    @IBAction func onTap(sender: AnyObject) {
+view.endEditing(true)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
